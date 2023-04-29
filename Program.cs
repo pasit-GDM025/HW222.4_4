c81ea0933290418264bbd96776a0a78570638ce8
pasit-GDM025 <pasit.pipat@mail.kmutt.ac.th>
2023-04-29 19:46:05 +0700

    ...

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+
+        List<int> nodes = new List<int>();
+        while (true)
+        {
+            int node1 = int.Parse(Console.ReadLine());
+            if(node1 >= x || node1 < 0)
+            {
+                break;
+            }
+            int node2 = int.Parse(Console.ReadLine());
+            if(node2 >= x || node2 < 0)
+            {
+                break;
+            }
+            if(node1 == node2)
+            {
+                break;
+            }
+            else
+            {
+                nodes.Add(node1);
+                nodes.Add(node2);
+            }
+        }
+
+        HashSet<int> usedSymbols = new HashSet<int>();
+        foreach (int node in nodes) 
+        {
+            usedSymbols.Add(node % x);
+        }
+        int Symbols = usedSymbols.Count - 2;
+        Console.WriteLine("Ans: {0}",Symbols);
+    }
+}
